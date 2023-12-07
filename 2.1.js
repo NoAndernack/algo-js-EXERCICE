f111937f5f184a62efba71cea50f43ef378c5aed
NoAndernack <andernackn@gmail.com>
2023-12-07 16:36:47 +0100

    exercice2-1

---
+++ 2.1.js
@@ -0,0 +1,9 @@
+var userAge = prompt("Please enter your age:");
+var age = userAge;
+
+// Check if the user is an adult
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are not yet an adult.");
+}
