6a537386653b9c362ba3b3921732bd1a5e07620d
NoAndernack <andernackn@gmail.com>
2023-12-07 14:54:04 +0100

    V1ex

---
+++ 1.0.js
@@ -0,0 +1,8 @@
+let addition = 23+5;
+let soustraction= 23-5;
+let multiplication=23*5;
+let division=23/5;
+let modulo=23%5;
+
+
+console.log(addition , soustraction, multiplication,division,modulo );
