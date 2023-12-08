2b109cec25aef97ac95440cb754e7f989fe96469
NoAndernack <andernackn@gmail.com>
2023-12-08 09:43:21 +0100

    ex2-7

---
+++ 2.7.js
@@ -0,0 +1,10 @@
+let n = Number (prompt('Donnez un chiffre'));
+let sum = 0 ;
+
+
+for (let i = 1; i<=n;i++){
+    let x = Number (prompt('Donnez un chiffre'));
+    sum+=x
+    
+}
+console.log(`La somme est ${sum}`)
