1ba009bac491aabfc6579cdc7a39bf437d9c2f8e
NoAndernack <andernackn@gmail.com>
2023-12-08 12:08:59 +0100

    ex4

---
+++ 3.4.js
@@ -0,0 +1,7 @@
+let tab = [2, 'a', 4, 'b', 6];
+let x = tab.length;
+
+
+    console.log(tab[0]);
+    console.log(tab[x-1]);
+
---
+++ 4.1.js
@@ -0,0 +1,8 @@
+let width = prompt('Donnes une largeur pour un rectangle');
+let height = prompt('Donnes une hauteur pour un rectangle');
+
+function calcSurface(width, height) {
+    return width * height;
+  }
+  
+  console.log(calcSurface(width, height));
