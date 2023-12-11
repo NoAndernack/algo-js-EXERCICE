c4e19ff7737d107406997a430e86b48ec0dedc15
NoAndernack <andernackn@gmail.com>
2023-12-11 09:10:10 +0100

    ex4_3

---
+++ 4.3.js
@@ -0,0 +1,85 @@
+
+// const tab=[]
+// function rand10() {
+//     return Math.floor(Math.random() * 10)+1;
+// }
+
+// const n= rand10();
+
+// console.log(n);
+
+    
+// for(let i=0 ; i < n; ) {
+
+//     tab.push(rand10());
+//     i++;
+// }
+
+// console.log(tab)
+
+// ---------------------------------------------------
+
+// const a = prompt('Donnez un nombre aléatoire')
+// const tab=[]
+
+// function rand10() {
+//     return Math.floor(Math.random() * 10)+1;
+// }
+// const n= rand10();
+// console.log(n);
+
+    
+
+
+// for(let i=0 ; i < a; ) {
+
+//     tab.push(rand10());
+//     i++;
+// }
+
+// console.log(tab)
+
+// _______________________________________
+
+// const a = prompt('Donnez un nombre aléatoire')
+// const tab=[]
+
+// function rand10() {
+//     return Math.floor(Math.random() * 10)+1;
+// }
+// const n= rand10();
+
+
+    
+
+// function multiRand(){
+// for(let i=0 ; i < a; ) {
+
+//     tab.push(rand10());
+//     i++;
+// }console.log(tab);
+// }
+// multiRand (tab);
+
+// ------------------------------------------------------------------------
+
+
+const tab=[]
+function rand10() {
+    return Math.floor(Math.random() * 10)+1;
+}
+
+const n= rand10();
+
+console.log(n);
+
+    
+function multiRand(){
+    for(let i=0 ; i < n; ) {
+    
+        tab.push(rand10());
+        i++;
+    }console.log(tab);
+    }
+    multiRand (tab);
+
